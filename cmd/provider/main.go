58d4f2b726fa875e54d53efa4405de51f35e8634
Tammy Young <tammy.q.young@gmail.com>
2024-07-02 12:15:01 -0400

    Revert "Revert logging format changes"
    This reverts commit 04387b3d27f9a49a0646ffaea14f3fda68040a22.

--- cmd/provider/main.go
+++ cmd/provider/main.go
@@ -64,7 +64,7 @@ func main() {
 	)
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 
-	zl := zap.New(zap.UseDevMode(*debug), UseISO8601())
+	zl := zap.New(zap.UseDevMode(*debug), UseJSONencoder())
 	log := logging.NewLogrLogger(zl.WithName("provider-terraform"))
 	if *debug {
 		// The controller-runtime runs with a no-op logger by default. It is
@@ -148,9 +148,11 @@ func main() {
 	kingpin.FatalIfError(mgr.Start(ctrl.SetupSignalHandler()), "Cannot start controller manager")
 }
 
-// UseISO8601 sets the logger to use ISO8601 timestamp format
-func UseISO8601() zap.Opts {
+// UseJSONencoder sets the logger to use json format for log records
+func UseJSONencoder() zap.Opts {
 	return func(o *zap.Options) {
-		o.TimeEncoder = zapcore.ISO8601TimeEncoder
+		encoderConfig := zapuber.NewProductionEncoderConfig()
+		encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+		o.Encoder = zapcore.NewJSONEncoder(encoderConfig)
 	}
 }
